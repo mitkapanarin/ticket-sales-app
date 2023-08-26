233137a40e944df3e2f2e695322a84bf463c1dcf
John Doe <mitka_j@yahoo.com>
2023-08-26 14:57:22 +0200

    starter files added

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ErrorPage, Home, Login, Signup } from './Pages'
+
+const App = () => {
+  return (
+<BrowserRouter>
+<Routes>
+  <Route path='/' element={<Home/>}/>
+  <Route path='/login' element={<Login/>}/>
+  <Route path='/signup' element={<Signup/>}/>
+  <Route path='*' element={<ErrorPage/>}/>
+</Routes>
+</BrowserRouter>
+  )
+}
+
+export default App
---
+++ frontend/src/Pages/index.tsx
@@ -0,0 +1,6 @@
+import Signup from "./Signup";
+import ErrorPage from "./ErrorPage";
+import Login from "./Login";
+import Home from "./Home";
+
+export {ErrorPage, Home, Login, Signup}
