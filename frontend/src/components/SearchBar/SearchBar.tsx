13deb0f561e6cd262a234b5529e940d238332d78
John Doe <mitka_j@yahoo.com>
2023-09-02 19:36:42 +0200

    searchBar added

--- frontend/src/components/SearchBar/SearchBar.tsx
+++ frontend/src/components/SearchBar/SearchBar.tsx
@@ -32,7 +32,7 @@ const SearchBar = () => {
             type="search"
             id="default-search"
             className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Search Mockups, Logos..."
+            placeholder="Search for concerts or comedies..."
           />
           <button
             type="button"
@@ -69,7 +69,7 @@ const SearchBar = () => {
                 <InputField
                   label=""
                   name="search"
-                  placeholder="Concerts, Comedyes, etc."
+                  placeholder="Search for concerts or comedies..."
                   type="text"
                   onChange={(e) => setSearch(e.target.value)}
                   value={search}
