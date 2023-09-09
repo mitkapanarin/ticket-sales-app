ea276b0db37f6f3ecbe86b0e3b34f616c6170e5e
John Doe <mitka_j@yahoo.com>
2023-09-09 13:55:37 +0200

    saveAnEventToBookMark and removeAnEventFromBookmark added to EventDetailsPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -27,11 +27,11 @@ const App = () => {
           <Route path="/musical-concerts" element={<MusicalConcerts />} />
           <Route path="/stand-up-comedies" element={<StandUpComedies />} />
           <Route path="/events/:id" element={<EventDetailsPage />} />
-          <Route path="/update-event/:id" element={<EditEventForm />} />
           <Route path="/bookmark" element={<BookMark />} />
           <Route path="/purchase-history" element={<PurchaseHistory />} />
           <Route element={<ProtectedRoutes />}>
             <Route path="/profile" element={<Profile />} />
+            <Route path="/update-event/:id" element={<EditEventForm />} />
           </Route>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
