eb062c3aae9d4ed0c04158087ee40fbb2da7283d
John Doe <mitka_j@yahoo.com>
2023-09-10 18:11:17 +0200

    search bar feature completed

--- frontend/src/Pages/DisplaySearchResult.tsx
+++ frontend/src/Pages/DisplaySearchResult.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useSearchEventsQuery } from "../store/API/EventsAPI";
+import { useSelector } from 'react-redux';
+
 
 const DisplaySearchResult = () => {
+    const searchTerm = useSelector<string>(state => state.SearchTerm.searchTerm);
+    // console.log(searchTerm)
+    const { data, error, isLoading } = useSearchEventsQuery(searchTerm);
+    console.log(data);
     return (
         <div>DisplaySearchResult</div>
     )
